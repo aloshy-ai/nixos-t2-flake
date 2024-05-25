2b54e3af934cc44b00363ed348f796f9d0f9a87f
Cassie Cheung <me@soopy.moe>
2024-05-26 00:20:54 +0800

    chore: fix missing flag

--- configuration.nix
+++ configuration.nix
@@ -5,7 +5,7 @@ throw ''
     nix flake init -t github:soopyc/nixos-t2-flake
 
   Delete this file and run the following command:
-    sudo nixos-generate-config --dir $PWD
+    sudo nixos-generate-config --dir $PWD --root /mnt
 
   You can then configure your system to your liking.
 ''
