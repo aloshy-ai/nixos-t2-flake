7d9e3dee4e3ddf8f97c544e076a4f9d9a883d403
Cassie Cheung <me@soopy.moe>
2024-12-09 13:53:53 +0800

    chore: modernize

--- configuration.nix
+++ configuration.nix
@@ -4,10 +4,10 @@ throw ''
   Do not attempt to build your system directly via this flake, as this error will be shown. Instead, initialize a local flake from this template:
     nix flake init -t github:soopyc/nixos-t2-flake
 
-  Delete this file and run the following command:
+  Delete this configuration.nix file and run the following command:
     sudo nixos-generate-config --dir $PWD --root /mnt
 
   You can then configure your system to your liking.
 
-  See the README.md document for more information.
+  See the README.md document in the template repo for more information.
 ''
--- flake.nix
+++ flake.nix
@@ -1,38 +1,46 @@
 {
   nixConfig = {
     extra-substituters = [
-      # "https://hydra.soopy.moe"
-      "https://cache.soopy.moe" # toggle these if this one doesn't work.
+      "https://cache.soopy.moe"
     ];
-    extra-trusted-public-keys =
-      [ "hydra.soopy.moe:IZ/bZ1XO3IfGtq66g+C85fxU/61tgXLaJ2MlcGGXU8Q=" ];
+    extra-trusted-public-keys = [ "cache.soopy.moe-1:0RZVsQeR+GOh0VQI9rvnHz55nVXkFardDqfm4+afjPo=" ];
   };
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
-    nixos-hardware.url = "github:nixos/nixos-hardware";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixos-hardware.url = "github:NixOS/nixos-hardware";
   };
 
-  outputs = { nixpkgs, nixos-hardware, ... }: {
-    # remove everything below this
-    templates.default = {
-      path = builtins.filterSource (path: _:
-        (!builtins.elem (builtins.baseNameOf path) [ "README.md" "configuration.nix" "LICENSE" ])) ./.;
-      description =
-        "A flake to quickly get started to use NixOS on a T2 Mac device.";
+  outputs =
+    { nixpkgs, nixos-hardware, ... }:
+    {
+      # ===== remove everything below this =====
+      templates.default = {
+        path = builtins.filterSource (
+          path: _:
+          (
+            !builtins.elem (builtins.baseNameOf path) [
+              "README.md"
+              "LICENSE"
+              "flake.lock"
+            ]
+          )
+        ) ./.;
+        description = "A flake to quickly get started to use NixOS on a T2 Mac device.";
+      };
+      # ===== remove everything above this =====
+
+      formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixfmt-rfc-style;
+
+      # replace yourHostname with your actual hostname!
+      nixosConfigurations.yourHostname = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+
+          ./nix/substituter.nix
+          nixos-hardware.nixosModules.apple-t2
+        ];
+      };
     };
-    # remove everything above this
-
-    formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixfmt-classic;
-
-    nixosConfigurations.yourHostname = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./configuration.nix
-
-        ./nix/substituter.nix
-        nixos-hardware.nixosModules.apple-t2
-      ];
-    };
-  };
 }
